c267ccaa59d96748dc52a1ac5053f0b7d2b85108
Rémino Rem <29999+remino@users.noreply.github.com>
2025-05-17 23:39:58 +0900

    Refactor tests, add tests for individual elements

--- tests/visual/anchor.test.js
+++ tests/visual/anchor.test.js
@@ -1,19 +1,14 @@
-import { test, expect } from '@playwright/test'
-import { dirname } from 'path'
-import { chromium } from 'playwright'
-import { fileURLToPath, pathToFileURL } from 'url'
+import { expect } from '@playwright/test'
+import { test } from '../setup.js'
 
 test('scroll position after navigating to targetted anchor on page', async ({
 	page,
 }) => {
-	const thisFile = fileURLToPath(import.meta.url)
-	const snapshotsDir = `${dirname(thisFile)}/snapshots`
-
-	await page.goto('http://localhost:4567/dress.css/elements/')
-	await page.click('a[href="#links"]')
+	await page.goto('http://localhost:4567/tests/anchor/')
+	await page.click('a[href="#second"]')
 	await page.waitForTimeout(1000)
 
 	const screenshot = await page.screenshot()
 
-	expect(screenshot).toMatchSnapshot(`anchor.png`)
+	expect(screenshot).toMatchSnapshot('anchor.png')
 })
--- tests/visual/elements.test.js
+++ tests/visual/elements.test.js
@@ -1,9 +1,116 @@
-import { test, expect } from '@playwright/test'
+import { expect } from '@playwright/test'
+import { test } from '../setup.js'
 
-test('visual regression of all styled elements on screen', async ({ page }) => {
-	await page.goto('http://localhost:4567/dress.css/elements/')
+const media = {
+	'screen-light': { colorScheme: 'light' },
+	'screen-dark': { colorScheme: 'dark' },
+	print: { media: 'print' },
+}
 
-	const screenshot = await page.screenshot({ fullPage: true })
+const directions = {
+	ltr: () => {},
+	rtl: () => {
+		document.documentElement.setAttribute('dir', 'rtl')
+	},
+	vrl: () => {
+		document.documentElement.style.writingMode = 'vertical-rl' // or 'vertical-lr'
+	},
+}
 
-	expect(screenshot).toMatchSnapshot('elements.png')
+const elements = {
+	abbr: {},
+	aside: {},
+	blockquote: {},
+	buttonlink: {},
+	buttons: {},
+	buttonset: {},
+	checkbox: {},
+	code: {},
+	color: {},
+	composite: {},
+	datetime: {},
+	details: {},
+	dialog: {},
+	dl: {},
+	download: {},
+	fieldset: {},
+	figure: {},
+	file: { maxDiffPixelRatio: 0.01 },
+	footer: {},
+	header: {},
+	headings: {},
+	hidden: {},
+	hr: {},
+	hyperlink: {},
+	inert: {},
+	inlinecode: {},
+	input: {},
+	kbd: {},
+	mark: {},
+	menu: {},
+	meter: {},
+	nav: {},
+	nestedfigures: {},
+	newwindow: {},
+	ol: {},
+	output: {},
+	pre: {},
+	precode: {},
+	progress: {},
+	radio: {},
+	range: {},
+	select: {},
+	singlefigure: {},
+	skipnav: {},
+	table: {},
+	textarea: {},
+	ul: {},
+	video: { maxDiffPixelRatio: 0.01 },
+}
+
+const pageEvals = page =>
+	Promise.all([
+		page.evaluate(() =>
+			Promise.all([
+				document.fonts.ready,
+				...Array.from(document.getAnimations()).map(anim => anim.finished),
+				() => {
+					const video = document.querySelector('video')
+					if (video) video.controls = false
+				},
+			])
+		),
+	])
+
+const navigate = async ({ dirFunc, elName, mediaProps, page }) => {
+	await page.goto(`http://localhost:4567/tests/elements/${elName}/`, {
+		waitUntil: 'domcontentloaded',
+	})
+
+	await page.emulateMedia(mediaProps)
+	await page.evaluate(dirFunc)
+	await pageEvals(page)
+
+	return page
+}
+
+test.describe.parallel('elements', () => {
+	Object.entries(elements)
+		.map(async ([elName, snapshotOpts]) => {
+			Object.entries(media).map(([mediaName, mediaProps]) => {
+				Object.entries(directions).map(([dirName, dirFunc]) => {
+					test(`${mediaName} ${dirName} ${elName}`, async ({ page }) => {
+						await navigate({ dirFunc, elName, mediaProps, page })
+
+						const screenshot = await page.screenshot({ fullPage: true })
+
+						expect(screenshot).toMatchSnapshot({
+							name: `${mediaName}-${dirName}-${elName}.png`,
+							...snapshotOpts,
+						})
+					})
+				})
+			})
+		})
+		.flat(2)
 })
