e28aab008e05e168d3bc197606d8ac049cbcd1ed
Rémino Rem <29999+remino@users.noreply.github.com>
2025-05-17 09:50:10 +0900

    Add banner comment in dress.css file

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,21 @@
+const pkg = require('./package.json')
+
 module.exports = {
 	plugins: [
+		require('postcss-banner')({
+			banner: `${pkg.name} v${pkg.version} | ${pkg.license} License | ${pkg.homepage || pkg.repository.url}`,
+			inline: true, // puts it at the top
+			important: true,
+		}),
 		require('cssnano')({
-			preset: require('cssnano-preset-default'),
+			preset: [
+				'default',
+				{
+					discardComments: {
+						remove: comment => !/^!/.test(comment), // keep /*! ... */
+					},
+				},
+			],
 		}),
 	],
 }
