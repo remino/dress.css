73ba01c386b049d3b7d25a2d2a210595f2f4c783
Rémino Rem <29999+remino@users.noreply.github.com>
2025-11-09 11:57:33 +0900

    Remove tests pages from build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,18 @@ import { defineConfig } from 'astro/config'
 import remarkCustomHeadingId from 'remark-custom-heading-id'
 import remarkToc from 'remark-toc'
 import nginxConfig from './src/integrations/nginxConfig.ts'
+import removeTests from './src/integrations/removeTests.ts'
 
 export default defineConfig({
 	output: 'static',
 	outDir: './deploy/public',
-	devToolbar: {
-		enabled: process.env.ASTRO_DEV_TOOLBAR_ENABLED !== '0',
-	},
-	markdown: {
+devToolbar: {
+	enabled: process.env.ASTRO_DEV_TOOLBAR_ENABLED !== '0',
+},
+build: {
+	exclude: ['/tests/*', '/tests/*/*'],
+},
+markdown: {
 		rehypePlugins: [],
 		remarkPlugins: [remarkCustomHeadingId, [remarkToc, { heading: 'Table of Contents' }]],
 	},
@@ -23,5 +27,6 @@ export default defineConfig({
 			template: 'src/nginx.conf.ejs',
 			output: 'nginx/dress.css.conf',
 		}),
+		removeTests(),
 	],
 })
